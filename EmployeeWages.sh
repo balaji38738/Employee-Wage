900fa45be271aefa785daf704285a97a0484ee7c
balaji38738 <bijjapwar123@gmail.com>
2020-03-14 18:11:54 +0530

    Stored employee wage in array

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -x
 
-#Problem Statement:- Calculate Wages till Number of Working Days or Total Working Hours per month is Reached
+#Problem Statement:- Calculate Wages till Number of Working Days or Total Working Hours per month is reached and store in array
 #Author:- Balaji Ijjapwar
 #Date:- 14 March 2020
 
@@ -27,10 +27,18 @@ function getWorkingHours() {
 	echo $workHours;
 }
 
+function calcDailyWage() {
+	local workHrs=$1
+	wage=$(( workHrs * EMP_RATE_PER_HR ))
+	echo $wage
+}
 while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	(( totalWorkingDays++ ));
 	workHours="$( getWorkingHours $(( RANDOM % 3 )) )";
 	totalWorkHours=$(( totalWorkHours + workHours ));
+	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workHours )"
 done
+
 totalSalary=$(( totalWorkHours * EMP_RATE_PER_HR ));
+echo "Daily Wage " ${empDailyWage[@]}
